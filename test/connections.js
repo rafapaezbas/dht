92f0bff6c28244caecb14bd9ddeb0037757a827d
rafapaezbas <rafaelpaezbastida@gmail.com>
2024-10-10 22:19:22 +0200

    added test for default bootstrap

--- test/connections.js
+++ test/connections.js
@@ -792,3 +792,50 @@ test('Populate DHT with options.nodes', async function (t) {
   a.destroy()
   b.destroy()
 })
+
+test.solo('createServer + connect - prod DHT', async function (t) {
+  t.plan(2)
+
+  const a = new DHT()
+  const b = new DHT()
+
+  t.teardown(() => {
+    a.destroy()
+    b.destroy()
+  })
+
+  const lc = t.test('socket lifecycle')
+
+  lc.plan(4)
+
+  const server = a.createServer(function (socket) {
+    lc.pass('server side opened')
+
+    socket.once('end', function () {
+      lc.pass('server side ended')
+      socket.end()
+    })
+  })
+
+  await server.listen()
+
+  const socket = b.connect(server.publicKey)
+
+  socket.once('open', function () {
+    lc.pass('client side opened')
+  })
+
+  socket.once('end', function () {
+    lc.pass('client side ended')
+  })
+
+  socket.end()
+
+  await lc
+
+  server.on('close', function () {
+    t.pass('server closed')
+  })
+
+  await server.close()
+})
